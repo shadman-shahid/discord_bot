2a78f041df3267df5e8ffd2c5017fb455637cc00
shadman-shahid <shadman9085@gmail.com>
2023-11-06 06:01:39 +0600

    Added new button for showing exam script

--- bot.py
+++ bot.py
@@ -62,18 +62,21 @@ async def get_assignment(user, assignment_no, drive_link, ctx=None, interaction=
 
 
     # student_id = '22301728'
-    student_id = re.search(r"[0-9]{8}", user.display_name).group(0)
-    file_id = 0
-    for (file_link, (i, filename)) in zip(file_links, file_id_names):
-        if student_id in filename:
-            file_id = i
-            desired_link = file_link
-            break
-    if file_id:
-        # Send the assignment file
-        await send_message(f"{user.mention}'s Assignment No. {assignment_no} is available at: {desired_link}", ephemeral=True)
-    else:
-        await send_message(
+    try:
+        student_id = re.search(r"[0-9]{8}", user.display_name).group(0)
+        file_id = 0
+        for (file_link, (i, filename)) in zip(file_links, file_id_names):
+            if student_id in filename:
+                file_id = i
+                desired_link = file_link
+                break
+        if file_id:
+            # Send the assignment file
+            await send_message(f"{user.mention}'s Assignment No. {assignment_no} is available at: {desired_link}", ephemeral=True)
+    except AttributeError:
+        pass
+    
+    await send_message(
             "Assignment not found! Perhaps you did not submit the assignment. Otherwise, contact faculty.", ephemeral=True)
 
 
@@ -110,8 +113,102 @@ async def return_assignments(ctx, assignment_no, drive_link):
     )
 
 
+async def get_exam_script(user, exam_type, drive_link, ctx=None, interaction=None):
+    async def send_message(*args, **kwargs):
+        if ctx:
+            await ctx.followup.send(*args, **kwargs)
+        else:
+            await interaction.response.send_message(*args, **kwargs)
+
+    """Extract the file names and google drive links of individual files in the drive folder URL."""
+
+    parsed_url = urlparse(drive_link)
+    if parsed_url.query:
+        try:
+            folder_id = parse_qs(parsed_url.query).get('id')[0]
+        except (TypeError):
+            path_segments = parsed_url.path.split('/')
+            folder_id = path_segments[-1] if len(path_segments) > 1 else None
+
+    file_list = drive.ListFile({'q': f"'{folder_id}' in parents and trashed=false"}).GetList()
+
+    # List of tuples of (file_id, file_name) type
+    file_id_names = [(file['id'], file['title']) for file in file_list]
+    file_links = [file.metadata.get('alternateLink') for file in file_list]
+
+    """Implement the logic here. I'm not sure, how student members are verified, in any case, that code will go here. 
+    For now, I am just using a randon student ID. """
 
 
+    # student_id = '22301728'
+    try:
+        student_id = re.search(r"[0-9]{8}", user.display_name).group(0)
+        file_id = 0
+        for (file_link, (i, filename)) in zip(file_links, file_id_names):
+            if student_id in filename:
+                file_id = i
+                desired_link = file_link
+                break
+        if file_id:
+            # Send the assignment file
+            await send_message(f"{user.mention}'s {exam_type} exam script has been checked and is available at: {desired_link}", ephemeral=True)
+    except AttributeError:
+        pass
+    
+    await send_message(
+            f"No {exam_type} exam script found. One of three scenarios are possible: \n 1. Your `{exam_type}` exam script has not been checked yet. \n2.  {user.mention} did not attend the exam. \n3. Some technical issue.", ephemeral=True)
+
+
+class SeeExamScriptButton(discord.ui.Button):
+    """
+    Represents a Discord UI button that allows users to see their exam script.
+
+    Attributes:
+        exam_type: The type of exam for which to fetch the script.
+        drive_link: The Google Drive link where the exam scripts are stored.
+    """
+    def __init__(self, exam_type, drive_link, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.exam_type = exam_type
+        self.drive_link = drive_link
+        self.label = f"Check {exam_type} answer script"
+        self.style = discord.ButtonStyle.red
+
+    async def callback(self, interaction: Interaction):
+        user = interaction.user
+        await get_exam_script(user, self.exam_type, self.drive_link, interaction=interaction)
+
+
+class SeeExamScriptButtonView(discord.ui.View):
+    """
+    Represents a Discord UI view that contains a SeeExamScriptButton.
+
+    Attributes:
+        exam_type: The type of exam for which to fetch the script.
+        drive_link: The Google Drive link where the exam scripts are stored.
+    """
+    
+    def __init__(self, exam_type, drive_link, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.timeout = None
+        self.add_item(item=SeeExamScriptButton(exam_type, drive_link))
+
+
+@bot.slash_command(name="show-scripts", description="Publish checked assignments with google drive link")
+async def show_scripts(ctx, exam_type, drive_link):
+    """
+    Asynchronously shows the checked assignments for a given exam type from a Google Drive link.
+
+    Args:
+        ctx: The context from which the function was called.
+        exam_type: The type of exam for which to fetch the script.
+        drive_link: The Google Drive link where the exam scripts are stored.
+    """
+    button_view = SeeExamScriptButtonView(exam_type, drive_link)
+    await ctx.respond(
+        f"## Press the button below to get your checked copy of `{exam_type}` exam script.",
+        view=button_view
+    )
 
 
 bot.run(token)
